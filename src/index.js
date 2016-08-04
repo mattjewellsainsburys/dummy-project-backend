3cfc946251a57e2d8bf105174db67cc680f2dd32
Matt Jewell <matt.jewell@sainsburys.co.uk>
2016-08-04 15:12:28 +0100

    remove dotenv

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 // this is where the magic happens
-require('dotenv').config();
 var express = require('express');
 var app = express();
-
-var port = process.env.LISTEN_ON_PORT || 3000;
-
 
 app.get('/now', function (req, res) {
   res.send(new Date());
 });
 
-
-app.listen(port);
-
-console.log('listening on: ' + port);
-
-setTimeout(function () {
-  console.log('I am bored now');
-  process.exit(1);
-}, 10000);
+app.set('port', process.env.PORT || 3000);
