44c7a92652258d3a612865bb061a9fd6d43f97df
Matt Jewell <matt.jewell@sainsburys.co.uk>
2016-08-04 16:15:50 +0100

    added trace id

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // this is where the magic happens
 var express = require('express');
+var uuid = require('uuid');
 var app = express();
+
+app.use(function (req, res, next) {
+  req.id = uuid.v4();
+  res.set('X-Trace-Id', req.id);
+  next();
+});
 
 app.get('/now', function (req, res) {
   res.send(new Date());
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1337);
 
 app.listen(app.get('port'));
 
